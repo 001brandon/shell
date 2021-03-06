598cb42da50f380a6bf0249bb47b4ef9c5b89b29
001brandon <brandon@LAPTOP-HRFEV0OI.localdomain>
2021-03-06 10:31:32 -0500

    Have the prompt print and replaced execve with execp, must change execp

---
+++ sh.h
@@ -0,0 +1,11 @@
+#include "get_path.h"
+
+int pid;
+char *which(char *command, struct pathelement *pathlist);
+void list(char *dir);
+void printenv(char **envp);
+void showprompt();
+
+#define PROMPTMAX 64
+#define MAXARGS   16
+#define MAXLINE   128
